18063edba4b95ec273d8c2ee8b9e3603afa5d935
Jeffrey Asmus <jeffreyasmus@Jeffreys-MacBook-Pro.local>
2016-09-10 16:04:26 -0700

    refactor game details api

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var rp = require('request-promise');
 var _ = require('lodash');
-const API_KEY = 'j4q82r8buyf3y2cxhthz9a94';
+const API_KEY = '3wrs7zd7ft7mzrg5uhhjd5pv';
 
 
 router.get('/', (req, res) => {
@@ -15,17 +15,17 @@ router.get('/', (req, res) => {
     }
 
     rp(options)
-    .then((result) => {
-        var schedule = {
-            data: JSON.parse(result)
-        }
+        .then((result) => {
+            var schedule = {
+                data: JSON.parse(result)
+            }
 
-        res.json(schedule);
+            res.json(schedule);
 
-    })
-    .catch((err) => {
-        res.json(err);
-    });
+        })
+        .catch((err) => {
+            res.json(err);
+        });
 
 });
 
@@ -39,69 +39,52 @@ router.get('/:id', (req, res) => {
     }
 
     rp(options)
-    .then((result) => {
-        var jsonResult = JSON.parse(result);
-        // console.log(jsonResult.games);
+        .then((result) => {
+            var jsonResult = JSON.parse(result);
 
-        var game = _.filter(jsonResult.games, function(g) {
-            if(g.id === req.params.id) {
-                return g;
+            var game = _.filter(jsonResult.games, function (g) {
+                if (g.id === req.params.id) {
+                    return g;
+                }
+            });
+
+            if (_.isEmpty(game)) {
+                res.sendStatus(404);
+            } else {
+                const gameDetail = game[0];
+                const year = req.query.year || 2016;
+                const week = req.query.week || 2;
+                const awayTeam = gameDetail.away;
+                const homeTeam = gameDetail.home;
+                var options2 = {
+                    uri: `http://api.sportradar.us/ncaafb-t1/${year}/REG/${week}/${awayTeam}/${homeTeam}/statistics.json`,
+                    qs: {
+                        api_key: API_KEY
+                    }
+                };
+
+                rp(options2)
+                    .then((result) => {
+                        var gameDetails = {
+                            data: JSON.parse(result)
+                        }
+
+                        res.json(gameDetails);
+
+                    })
+                    .catch((err) => {
+                        res.json(err);
+                    });
             }
+
+        })
+        .catch((err) => {
+            res.json(err);
         });
-        console.log('game');
-        console.log(game);
-        
-        if(_.isEmpty(game)) {
-            res.sendStatus(404);
-        } else {
-            res.json(game[0]);
-        }
-
-        
-
-//         var users = [
-//   { 'user': 'barney', 'age': 36, 'active': true },
-//   { 'user': 'fred',   'age': 40, 'active': false }
-// ];
- 
-// _.filter(users, function(o) { return !o.active; });
-
-
-    })
-    .catch((err) => {
-        res.json(err);
-    });
 
 });
 
-router.get('/details', (req, res) => {
-    const homeTeam = req.query.hometeam;
-    const awayTeam = req.query.awayteam;
-    const week = req.query.week || 2;
-    const year = req.query.year || 2016;
 
-    var options = {
-        uri: `http://api.sportradar.us/ncaafb-t1/${year}/REG/${week}/${awayTeam}/${homeTeam}/statistics.json`,
-        qs: {
-            api_key: API_KEY
-        }
-    };
-    console.log(options.uri);
-
-     rp(options)
-    .then((result) => {
-        console.log(result);
-        var gameDetails = {
-            data: JSON.parse(result)
-        }
-
-        res.json(gameDetails);
-
-    })
-    .catch((err) => {
-        res.json(err);
-    });
-});
 
 router.get('/roster', (req, res) => {
     const weekNumber = req.query.week || 2;
@@ -116,17 +99,17 @@ router.get('/roster', (req, res) => {
     }
 
     rp(options)
-    .then((result) => {
-        var roster = {
-            data: JSON.parse(result)
-        }
+        .then((result) => {
+            var roster = {
+                data: JSON.parse(result)
+            }
 
-        res.json(roster);
+            res.json(roster);
 
-    })
-    .catch((err) => {
-        res.json(err);
-    });
+        })
+        .catch((err) => {
+            res.json(err);
+        });
 
 });
 
