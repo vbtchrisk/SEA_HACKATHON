13122a2b53feb1b3c1c420fda28bb9f979f147ad
Jeffrey Asmus <jeffreyasmus@Jeffreys-MacBook-Pro.local>
2016-09-10 17:01:03 -0700

    clock api;

--- src/api/index.js
+++ src/api/index.js
@@ -84,6 +84,66 @@ router.get('/:id', (req, res) => {
 
 });
 
+router.get('/:id/clock', (req, res) => {
+   
+
+    const weekNumber = req.query.week || 2;
+    var options = {
+        uri: `http://api.sportradar.us/ncaafb-t1/2016/REG/${weekNumber}/schedule.json`,
+        qs: {
+            api_key: API_KEY
+        }
+    }
+
+    rp(options)
+        .then((result) => {
+            var jsonResult = JSON.parse(result);
+
+            var game = _.filter(jsonResult.games, function (g) {
+                if (g.id === req.params.id) {
+                    return g;
+                }
+            });
+
+            if (_.isEmpty(game)) {
+                res.sendStatus(404);
+            } else {
+                const gameDetail = game[0];
+                const year = req.query.year || 2016;
+                const week = req.query.week || 2;
+                const awayTeam = gameDetail.away;
+                const homeTeam = gameDetail.home;
+                var options2 = {
+                    uri: `http://api.sportradar.us/ncaafb-t1/${year}/REG/${week}/${awayTeam}/${homeTeam}/pbp.json`,
+                    qs: {
+                        api_key: API_KEY
+                    }
+                };
+
+                rp(options2)
+                    .then((result1) => {
+                       var jsonResult = JSON.parse(result1);
+                       const quarterIdx = jsonResult.quarters.length - 1;
+                       const pbpIdx = jsonResult.quarters[quarterIdx].pbp.length - 1;
+                       const actionIdx = jsonResult.quarters[quarterIdx].pbp[pbpIdx].actions.length - 1;
+                       var returnResponse = {
+                           clockValue: jsonResult.quarters[quarterIdx].pbp[pbpIdx].actions[actionIdx].clock,
+                           quarterValue: jsonResult.quarters[quarterIdx].number
+                       }
+
+                       res.json(returnResponse);
+                    })
+                    .catch((err) => {
+                        res.json(err);
+                    });
+            }
+
+        })
+        .catch((err) => {
+            res.json(err);
+        });
+});
+
 
 
 router.get('/roster', (req, res) => {
